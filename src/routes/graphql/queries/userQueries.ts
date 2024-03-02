3711ec702808befd8d670df6414937642bc7fd5d
Ryhor Volsasu <g.volosov@email.com>
2024-03-02 13:18:41 +0300

    feat: add query for user

---
+++ src/routes/graphql/queries/userQueries.ts
@@ -0,0 +1,19 @@
+import { prismaClient } from '../prismaClient.js';
+import { User } from '../type/types.js';
+import { UserType } from '../types/user.js';
+import { UsersType } from '../types/users.js';
+import { UUIDType } from '../types/uuid.js';
+
+export const UserQueries = {
+  user: {
+    type: UserType,
+    args: { id: { type: UUIDType } },
+    resolve: async (_obj, { id }: User) =>
+      await prismaClient.user.findFirst({ where: { id } }),
+  },
+
+  users: {
+    type: UsersType,
+    resolve: async () => await prismaClient.user.findMany(),
+  },
+};
