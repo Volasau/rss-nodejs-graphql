15c9654596f544a6e476f80af9b42d210da0677d
Ryhor Volsasu <g.volosov@email.com>
2024-03-02 13:35:43 +0300

    feat: add all queries in rootQuery

--- src/routes/graphql/rootQuery.ts
+++ src/routes/graphql/rootQuery.ts
@@ -1,6 +1,15 @@
 import { GraphQLObjectType } from 'graphql';
+import { userQueries } from './queries/userQueries.js';
+import { profileQueries } from './queries/profileQueries.js';
+import { memberTypeQueries } from './queries/memberQueries.js';
+import { postQueries } from './queries/postQueries.js';
 
 export const rootQuery = new GraphQLObjectType({
   name: 'rootQuery',
-  fields: () => ({}),
+  fields: () => ({
+    ...userQueries,
+    ...profileQueries,
+    ...postQueries,
+    ...memberTypeQueries,
+  }),
 });
