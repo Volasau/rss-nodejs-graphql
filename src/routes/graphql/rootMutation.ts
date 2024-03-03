bb709b985d92b4fcdc53076bfe2f4da0c2a0ca27
Ryhor Volsasu <g.volosov@email.com>
2024-03-03 18:42:22 +0300

    feat: add all mutation in rootMutation

--- src/routes/graphql/rootMutation.ts
+++ src/routes/graphql/rootMutation.ts
@@ -1,6 +1,13 @@
 import { GraphQLObjectType } from 'graphql';
+import { userMutation } from './mutation/userMutation.js';
+import { postMutation } from './mutation/postMutation.js';
+import { profileMutation } from './mutation/profileMutation.js';
 
 export const rootMutation = new GraphQLObjectType({
   name: 'rootMutation',
-  fields: () => ({}),
+  fields: () => ({
+    ...userMutation,
+    ...postMutation,
+    ...profileMutation,
+  }),
 });
